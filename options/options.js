eee882be8a1f269ee762c3f3d211115a3bb1bb4d
Crowdbotics <team@crowdbotics.com>
2021-08-30 08:26:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "top_grade_30194Identifier",
+  url: "https://top_grade_30194Identifier.botics.co",
+  api: "https://top_grade_30194Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
